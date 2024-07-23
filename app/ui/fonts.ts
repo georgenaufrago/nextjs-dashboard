f6227fe9ef5126a683ddaddeb92c976bf85d2a22
Georgy Voskanyan <georg.ab@gmx.at>
2024-07-23 13:52:12 +0200

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Montserrat } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] }); 
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+export const montserrat = Montserrat({ weight: ['400', '700'], subsets: ['cyrillic', 'latin']});
